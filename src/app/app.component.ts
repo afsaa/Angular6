eea22fcbc97133bbec0eb0d476cd5e706d89d82b
unknown <afsaa@utp.edu.co>
2018-09-20 12:53:33 -0500

    Listas de heroes dinámicas ngIf y enlaces de clases.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'tutorial-ToH';
+  title = "Tour de Héroes";
+  subtitle =
+    "Bienvenido al tour de los héroes. Aquí podrás ver sus características y habilidades.";
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./selectedHero";
+
+export const HEROES: Hero[] = [
+  {
+    id: 0,
+    name: "Garen",
+    habilities: ["Grito demaciano", "Fuerza de demacia"]
+  },
+  { id: 1, name: "Nocturne", habilities: ["Huella mortifera"] },
+  { id: 2, name: "Renekton", habilities: ["Trocear"] },
+  { id: 3, name: "Jax", habilities: ["Salto mortal"] },
+  { id: 4, name: "Kindred", habilities: [] },
+  { id: 5, name: "Pantheon", habilities: ["Falange!"] },
+  { id: 6, name: "Lux", habilities: [] }
+];
